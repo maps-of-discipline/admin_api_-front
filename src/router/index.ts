abeac78e4035b5d14b4444a66a4ddbec450029a3
George <y.savidi@yandex.ru>
2025-04-21 15:33:56 +0300

    на вкладке появились обозначения активных страниц

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,32 +11,43 @@ const routes = [
     path: '/',
     name: 'Login',
     component: LoginPage,
+    meta: { title: "Авторизация" }
   },
   {
     path: '/user',
     name: 'UserWork',
     component: UserWork,
+    meta: { title: "Пользователи" }
   },
   {
     path: '/loginServise',
     name: 'AuthService',
     component: AuthService,
+    meta: { title: "Сервис авторизации" }
   },
   {
     path: '/loginServise/email',
     name: 'AuthServiceByEmail',
     component: AuthServiceByEmail,
+    meta: { title: "Авторизация по Email" }
   },
   // {
   //   path: '/user/permissions',
   //   name: 'PermissionsWork',
   //   component: PermissionsWork,
+  //   meta: { title: "Права доступа" }
   // },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL || '/'),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const defaultTitle = "Мое приложение";
+  document.title = (to.meta.title as string) || defaultTitle;
+  next();
 });
 
 export default router;
